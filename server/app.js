5fddf71225e48a4abe20f926910b064afbb49652
IsaacMXL <isaacbarquero22@gmail.com>
2022-09-23 19:39:50 -0500

    Prueba #3

--- server/app.js
+++ server/app.js
@@ -1,5 +1,15 @@
-
+function prueba(){
+    console.log("Prueba!!!");
+}
 
 setTimeout(() => {
-    console.log("Prueba"); 
+    console.log("Hola mundo")
 }, 500);
+
+
+
+// prueba #3 xdxdxd
+
+const funcion = () => {
+    console.log("K biba la interfaz grafica :D");
+}
