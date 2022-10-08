8e6c9c6ad283c3672563f95356549d59841ab1a4
Daniel Penado <penadodan02@gmail.com>
2022-10-08 12:10:46 -0600

    Server configurado y listo para empezar a desarrollar APIs

---
+++ server/app.js
@@ -0,0 +1,38 @@
+const dotEnv = require("dotenv");
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const { request, response } = require("express");
+const cors = require("cors");
+const logger = require("morgan");
+const app = express();
+const prisma = new PrismaClient();
+
+
+//Archivos de Rutas
+
+
+//Acceder a la configuracion del archivo .env
+dotEnv.config();
+
+//Puerto que escucha por defecto 3000 o definido en .env
+const port = process.env.port || 3000;
+
+//Middleware CORS para aceptar llamadas en el servidor
+app.use(cors());
+//Middleware para loggear llamadas al servidor
+app.use(logger("dev"));
+//Middleware para gestionar Requests y Response JSON
+app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+
+//Definicion de Rutas
+
+//Servidor
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+    console.log("Presione CTRL-C para detenerlo\n");
+})
