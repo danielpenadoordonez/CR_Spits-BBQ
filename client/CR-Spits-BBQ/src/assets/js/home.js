21b24ae327c26b1c140cb0716fae61e60216dac9
isaacbarquero22MXL <isaacbarquero22@gmail.com>
2022-10-09 20:34:29 -0500

    Se creó el componente home-index dentro del módulo home, se agrego en assets/js el script home.js y swiper-bundle.min.js. Se agregó en carpeta assets/css el archivo swiper-bundle.css y se hizo referencia a su ruta dentro angular.json. Se agregó tres imágenes dentro de assets/images/products.

---
+++ client/CR-Spits-BBQ/src/assets/js/home.js
@@ -0,0 +1,11 @@
+
+let homeSwiper = new Swiper(".home-swiper", {
+    spaceBetween: 30,
+    loop: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+});
+console.log("llamada")
+
