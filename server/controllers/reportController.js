0983802cbfa676a11ab97c63ece485e20c3806d2
Daniel Penado <penadodan02@gmail.com>
2022-12-07 22:32:11 -0600

    Tercer API de reportes

--- server/controllers/reportController.js
+++ server/controllers/reportController.js
@@ -7,7 +7,7 @@ module.exports.ventasPorFecha = async (request, response, next) => {
     let fechaCierre = request.body.fechaCierre;
 
     if (fechaInicio !== undefined) {
-        if(fechaCierre === undefined){
+        if (fechaCierre === undefined) {
             //* Si no viene la fecha de cierre entonces se toma la fecha actual
             fechaCierre = getCurrentDate();
         }
@@ -40,7 +40,7 @@ module.exports.ventasPorMedioPago = async (request, response, next) => {
     let fechaCierre = request.body.fechaCierre;
 
     if (fechaInicio !== undefined) {
-        if(fechaCierre === undefined){
+        if (fechaCierre === undefined) {
             //* Si no viene la fecha de cierre entonces se toma la fecha actual
             fechaCierre = getCurrentDate();
         }
@@ -63,6 +63,93 @@ module.exports.ventasPorMedioPago = async (request, response, next) => {
                         GROUP BY t.descripcion`
         );
         response.json(reportData);
+    }
+}
+
+module.exports.ventasPorMesa = async (request, response, next) => {
+    //* Casteo los parámetros
+    let fechaInicio = request.body.fechaInicio;
+    let fechaCierre = request.body.fechaCierre;
+    let mesaID = parseInt(request.body.mesa);
+    //Validar que haya enviado una mesa en el request
+    if (mesaID === undefined) {
+        response.status(400).json({
+            success: false,
+            message: "Mesa no especificada"
+        })
+    }
+    else {
+        const mesa = await prisma.mesa.findFirst({
+            where: { id: mesaID },
+          });
+
+        if (fechaInicio !== undefined) {
+            if (fechaCierre === undefined) {
+                //* Si no viene la fecha de cierre entonces se toma la fecha actual
+                fechaCierre = getCurrentDate();
+            }
+            //* Cantidad de productos vendidos x fecha
+            const reportData = await prisma.$queryRaw(
+                Prisma.sql`SELECT idMesa as Mesa
+                            FROM Pedido
+                            WHERE fecha BETWEEN ${fechaInicio} AND ${fechaCierre} AND idMesa = ${mesaID}`
+            );
+            response.json({"Mesa" : mesa.codigo, "Cantidad" : reportData.length});
+        }
+        else {
+            //* Si no se especifican fechas entonces por defecto se busca por la fecha actual
+            let currentDate = getCurrentDate();
+            const reportData = await prisma.$queryRaw(
+                Prisma.sql`SELECT idMesa as Mesa
+                            FROM Pedido
+                            WHERE fecha = ${currentDate} AND idMesa = ${mesaID}`
+            );
+            response.json({"Mesa" : mesa.codigo, "Cantidad" : reportData.length});
+        }
+    }
+}
+
+
+module.exports.ventasPorMesero = async (request, response, next) => {
+    //* Casteo los parámetros
+    let fechaInicio = request.body.fechaInicio;
+    let fechaCierre = request.body.fechaCierre;
+    let meseroID = request.body.mesero;
+    //Validar que haya enviado una mesa en el request
+    if (meseroID === undefined) {
+        response.status(400).json({
+            success: false,
+            message: "Mesero no especificado"
+        })
+    }
+    else {
+        const mesero = await prisma.usuario.findFirst({
+            where: { id: meseroID },
+          });
+
+        if (fechaInicio !== undefined) {
+            if (fechaCierre === undefined) {
+                //* Si no viene la fecha de cierre entonces se toma la fecha actual
+                fechaCierre = getCurrentDate();
+            }
+            //* Cantidad de productos vendidos x fecha
+            const reportData = await prisma.$queryRaw(
+                Prisma.sql`SELECT idMesero as Mesero
+                            FROM Pedido
+                            WHERE fecha BETWEEN ${fechaInicio} AND ${fechaCierre} AND idMesero = ${meseroID}`
+            );
+            response.json({"Mesero" : mesero.id, "Cantidad" : reportData.length});
+        }
+        else {
+            //* Si no se especifican fechas entonces por defecto se busca por la fecha actual
+            let currentDate = getCurrentDate();
+            const reportData = await prisma.$queryRaw(
+                Prisma.sql`SELECT idMesero as Mesero
+                            FROM Pedido
+                            WHERE fecha = ${currentDate} AND idMesero = ${meseroID}`
+            );
+            response.json({"Mesero" : mesero.id, "Cantidad" : reportData.length});
+        }
     }
 }
 
