335046cebac35813971a63c18b01fc848c0951cc
= <maritoxd1@gmail.com>
2022-12-08 16:06:59 -0600

    Avances

--- client/CR-Spits-BBQ/src/app/pedidos/reporte-fechas/reporte-fechas.component.ts
+++ client/CR-Spits-BBQ/src/app/pedidos/reporte-fechas/reporte-fechas.component.ts
@@ -84,27 +84,44 @@ export class ReporteFechasComponent implements AfterViewInit {
     }
     this.grafico = new Chart(this.ctx, {
 
-      type: 'pie',
+      type: 'bar',
       data: {
         //* Etiquetas del grafico, debe ser un array
         labels: this.datos.map(x => x.fecha),
         //* Isaac puede cambiar los colores a gusto...
         datasets: [
           {
-            backgroundColor: [
-              '#2ecc71',
-              '#3498db',
-              '#95a5a6',
-              '#9b59b6',
-              '#f1c40f',
-              '#e74c3c',
-            ],
-            //* Datos del grafico, debe ser un array
+            label: "Fecha",
             data: this.datos.map(x => x.cantidadProductos),
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+              'rgba(255, 205, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(201, 203, 207, 0.2)'
+            ],
+            borderColor: [
+              'rgb(255, 99, 132)',
+              'rgb(255, 159, 64)',
+              'rgb(255, 205, 86)',
+              'rgb(75, 192, 192)',
+              'rgb(54, 162, 235)',
+              'rgb(153, 102, 255)',
+              'rgb(201, 203, 207)'
+            ],
+            borderWidth: 1,
+            //* Datos del grafico, debe ser un array
           },
         ],
       },
       options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        },
         responsive: false,
         maintainAspectRatio: false,
       },
